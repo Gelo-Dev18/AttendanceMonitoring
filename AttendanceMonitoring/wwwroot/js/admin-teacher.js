51500d42ca749945a672b01d370745a5f1d049b5
dayo <michaelangelopimentel18@gmail.com>
2025-10-22 18:06:44 +0800

    Move the reusable js code to site.js, and modify and rename the admin.js file to admin-teacher.js for optimal file structure

--- AttendanceMonitoring/wwwroot/js/admin-teacher.js
+++ AttendanceMonitoring/wwwroot/js/admin-teacher.js
@@ -1,55 +1,6 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
 
-    var teacherID;
-    //Event delegation for file input change
-    $(document).on('change', 'input[type="file"].custom-file-input', function () {
-        var input = this;
-        //Update the custom file label with selected filename
-        var fileName = input.files[0] ? input.files[0].name : 'Choose file'; //ternary operator(shorthand for if else)
-        var label = $(input).next('.custom-file-label');
-        label.text(fileName);
-
-        //Display image when files is selected
-        if (input.files && input.files[0]) { //0 Indicates always for true
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                //Find the image in the same modal/container
-                $(input).closest('.modal-content, .container').find('#imagePreview').attr('src', e.target.result);
-            }
-            reader.readAsDataURL(input.files[0]);
-        }
-    });
-    ///////////////////////////////////////////////////
-
-    //Triggers Add user modal
-    $('#AddModalForm').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget);
-
-        var url = button.data('url');
-        var title = button.data('title');
-
-        var modal = $(this);
-
-        modal.find('#AddModalFormLabel').text(title);
-        loadBlurBackground();
-        $.ajax({
-            url: url,
-            type: 'GET',
-            success: function (html) {
-                modal.find('#AddModalFormBody').html(html);
-            },
-            error: function (xhr, status, error) {
-                console.error('Error loading add teacher modal:', error);
-                modal.find('#AddModalFormBody').html('<p class="text-center">Failed to load modal body</p>');
-            }
-        });
-    });
-
-    $('#AddModalForm').on('hide.bs.modal', function () {
-        hideBlurBackground();
-    });
-
-    //Submit Function For Adding
+    //Submit Function For Adding Teacher
     $(document).on('submit', '#AddTeacherForm', function (e) {
         e.preventDefault(); // stops the default page refresh/navigation on form submission, allowing JavaScript to handle the data submission.
 
@@ -121,42 +72,6 @@
     });
     //////////////////////////////////////////////////////////////
 
-    //Triggers Edit Modal
-    $('#EditModal').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget);
-
-        teacherID = button.data('id');
-
-        var url = button.data('url');
-        var title = button.add('title');
-
-        var modal = $(this);
-        modal.find('#EditModalLabel').text(title);
-
-        loadBlurBackground();
-
-        if (!teacherID) {
-            alert('Id does not found');
-            return;
-        }
-
-        $.ajax({
-            url: url + teacherID,
-            type: 'GET',
-            success: function (html) {
-                modal.find('#EditModalBody').html(html);
-            },
-            error: function (xhr, status, error) {
-                console.error('Error loading edit teacher modal:', error);
-                modal.find('#EditModalBody').html('<p class="text-center">Failed to Load modal body</p>');
-            }
-        });      
-    });
-
-    $('#EditModal').on('hide.bs.modal', function () {
-        hideBlurBackground();
-    });
-
     //Submit Function for Edit Teacher
     $(document).on('submit', '#EditTeacherForm', function (e) {
         e.preventDefault();
@@ -216,57 +131,7 @@
             }
         });
     });
-    ///////////////////////////////////////////////////////////////////////////
-
-    //BEst approach Separate Function with Parameters (soc)"Separation of Concerns"
-            //yung function, dapat tumatanggap siya ng parameters
-    function loadViewModal(url, teacherID, modal) {
-        $.ajax({
-            url: url + teacherID,
-            type: 'GET',
-            success: function (html) {
-                modal.find('#ViewModalBody').html(html);
-            },
-            error: function (xhr, status, error) {
-                console.error('Error loading add teacher modal:', error);
-                modal.find('#AddModalFormBody').html('<p class="text-center">Failed to load modal body</p>');
-            }
-        });
-    }
-
-    //Trigger View Modal
-    $('#ViewModal').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget);
-        teacherID = button.data('id');
-
-        var url = button.data('url');
-        var title = button.data('title');
-
-        var modal = $(this);
-
-        modal.find('#ViewModalLabel').text(title);
-
-        loadBlurBackground();
-        // Ipasa 'yung local variables sa function
-        loadViewModal(url, teacherID, modal);
-    });
-
-    $('#ViewModal').on('hide.bs.modal', function () {
-        hideBlurBackground();
-    });
-    //////////////////////////////////////////////////////////////////////////////
-
-    //Triggers ModalDelete
-    $('#ModalDelete').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget);
-        teacherID = button.data('id');
-
-        loadBlurBackground();
-    });
-
-    $('#ModalDelete').on('hide.bs.modal', function (event) {
-        hideBlurBackground();
-    });
+    ///////////////////////////////////////////////////////////////////////////////
 
     $('#confirmDeleteButton').on('click', function (e) {
         e.preventDefault();
@@ -309,35 +174,4 @@
     });
     ///////////////////////////////////////////////////////////////////////////
 
-    //Useful Functions
-    function showSuccessToast(message) {
-        //$('#successToast .toast-body').text(message);
-        $('#toast-message').text(message);
-        $('#successToast').toast('show');
-    }
-
-    function showUpdateSuccessToast(message) {
-        $('#update-toast-message').text(message);
-        $('#updateSuccessToast').toast('show');
-    }
-
-    function showDangerToast() {
-        $('#dangerToast').toast('show');
-    }
-
-    function loadSpinner() {
-        $('#spinnerWrapper').removeClass("d-none");
-    }
-
-    function loadPageBlur() {
-        $('#blurred-overlay-page').css('display', 'block');
-    }
-
-    function loadBlurBackground() {
-        $('#blurred-overlay').css('display', 'block');
-    }
-
-    function hideBlurBackground() {
-        $('#blurred-overlay').css('display', 'none');
-    }
 });
--- AttendanceMonitoring/wwwroot/js/site.js
+++ AttendanceMonitoring/wwwroot/js/site.js
@@ -1,4 +1,175 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var teacherID;
+var secretaryID;
+var studentID;
+//Reusable Codes
+function showSuccessToast(message) {
+    //$('#successToast .toast-body').text(message);
+    $('#toast-message').text(message);
+    $('#successToast').toast('show');
+}
+function showUpdateSuccessToast(message) {
+    $('#update-toast-message').text(message);
+    $('#updateSuccessToast').toast('show');
+}
 
-// Write your JavaScript code.
+function showDangerToast() {
+    $('#dangerToast').toast('show');
+}
+
+function loadSpinner() {
+    $('#spinnerWrapper').removeClass("d-none");
+}
+
+function loadPageBlur() {
+    $('#blurred-overlay-page').css('display', 'block');
+}
+
+function loadBlurBackground() {
+    $('#blurred-overlay').css('display', 'block');
+}
+
+function hideBlurBackground() {
+    $('#blurred-overlay').css('display', 'none');
+}
+//BEst approach Separate Function with Parameters (soc)"Separation of Concerns"
+//yung function, dapat tumatanggap siya ng parameters
+function loadViewModal(url, teacherID, modal) {
+    $.ajax({
+        url: url + teacherID,
+        type: 'GET',
+        success: function (html) {
+            modal.find('#ViewModalBody').html(html);
+        },
+        error: function (xhr, status, error) {
+            console.error('Error loading add teacher modal:', error);
+            modal.find('#AddModalFormBody').html('<p class="text-center">Failed to load modal body</p>');
+        }
+    });
+}
+
+$(document).ready(function () {
+
+    //Event delegation for file input change
+    $(document).on('change', 'input[type="file"].custom-file-input', function () {
+        var input = this;
+        //Update the custom file label with selected filename
+        var fileName = input.files[0] ? input.files[0].name : 'Choose file'; //ternary operator(shorthand for if else)
+        var label = $(input).next('.custom-file-label');
+        label.text(fileName);
+
+        //Display image when files is selected
+        if (input.files && input.files[0]) { //0 Indicates always for true
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                //Find the image in the same modal/container
+                $(input).closest('.modal-content, .container').find('#imagePreview').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    });
+
+    ///////////////////////////////////////////////////////////////////////////////
+
+    //Triggers Add user modal
+    $('#AddModalForm').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+
+        var url = button.data('url');
+        var title = button.data('title');
+
+        var modal = $(this);
+
+        modal.find('#AddModalFormLabel').text(title);
+        loadBlurBackground();
+        $.ajax({
+            url: url,
+            type: 'GET',
+            success: function (html) {
+                modal.find('#AddModalFormBody').html(html);
+            },
+            error: function (xhr, status, error) {
+                console.error('Error loading add teacher modal:', error);
+                modal.find('#AddModalFormBody').html('<p class="text-center">Failed to load modal body</p>');
+            }
+        });
+    });
+
+    $('#AddModalForm').on('hide.bs.modal', function () {
+        hideBlurBackground();
+    });
+
+    ///////////////////////////////////////////////////////////////////////////////
+
+    //Triggers Edit Modal
+    $('#EditModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+
+        teacherID = button.data('id');
+
+        var url = button.data('url');
+        var title = button.data('title');
+
+        var modal = $(this);
+        modal.find('#EditModalLabel').text(title);
+
+        loadBlurBackground();
+
+        if (!teacherID) {
+            alert('Id does not found');
+            return;
+        }
+
+        $.ajax({
+            url: url + teacherID,
+            type: 'GET',
+            success: function (html) {
+                modal.find('#EditModalBody').html(html);
+            },
+            error: function (xhr, status, error) {
+                console.error('Error loading edit teacher modal:', error);
+                modal.find('#EditModalBody').html('<p class="text-center">Failed to Load modal body</p>');
+            }
+        });
+    });
+
+    $('#EditModal').on('hide.bs.modal', function () {
+        hideBlurBackground();
+    }); 
+
+    ///////////////////////////////////////////////////////////////////////////////
+
+    //Trigger View Modal
+    $('#ViewModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+        teacherID = button.data('id');
+
+        var url = button.data('url');
+        var title = button.data('title');
+
+        var modal = $(this);
+
+        modal.find('#ViewModalLabel').text(title);
+
+        loadBlurBackground();
+        // Ipasa 'yung local variables sa function
+        loadViewModal(url, teacherID, modal);
+    });
+
+    $('#ViewModal').on('hide.bs.modal', function () {
+        hideBlurBackground();
+    });
+
+    ///////////////////////////////////////////////////////////////////////////////
+
+    //Triggers ModalDelete
+    $('#ModalDelete').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+        teacherID = button.data('id');
+
+        loadBlurBackground();
+    });
+
+    $('#ModalDelete').on('hide.bs.modal', function (event) {
+        hideBlurBackground();
+    });
+});
